f8982b2b721f63e22c14ed5665c58e31821c70d7
Kevin Fernandez <kevinfernandeznueve9@gmail.com>
2026-01-07 16:51:37 +0100

    changement de logique pour le chargement des requêtes api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,28 +33,26 @@ export default function Home() {
     const fetchDataApi = async () => {
       try {
         const topAnimesData = await getTopAnimes();
-        await delay(600);
-
-        const seasonAnimesData = await getSeasonAnimes();
+        setTopAnimes(topAnimesData)
         await delay(600);
 
         const popularAnimesData = await getPopularAnimes();
+        setPopularAnimes(popularAnimesData);
+        await delay(600);
+
+        const seasonAnimesData = await getSeasonAnimes();
+        setSeasonAnimes(seasonAnimesData);
         await delay(600);
 
         const actionAnimesData = await getActionAnimes();
+        setActionAnimes(actionAnimesData);
         await delay(600);
 
         const romanceAnimesData = await getRomanceAnimes();
+        setRomanceAnimes(romanceAnimesData);
         await delay(600);
 
         const comedyAnimesData = await getComedyAnimes();
-
-
-        setTopAnimes(topAnimesData);
-        setSeasonAnimes(seasonAnimesData);
-        setPopularAnimes(popularAnimesData);
-        setActionAnimes(actionAnimesData);
-        setRomanceAnimes(romanceAnimesData);
         setComedyAnimes(comedyAnimesData);
       } catch (error) {
         console.error("Erreur lors du chargement des animes:", error);
@@ -69,19 +67,19 @@ export default function Home() {
 
   //   console.log(topAnimes);
 
-  if (loading) return <p>Chargement...</p>;
+  // if (loading) return <p>Chargement...</p>;
   if (error) return <p>Erreur API Jikan</p>;
 
 
   return (
     <>
       <HeroSlider animes={popularAnimes} />
+      <AnimeSection titleSection="Les plus populaires" data={popularAnimes} />
       <AnimeSection titleSection="Top animes" data={topAnimes} />
       <AnimeSection titleSection="Action" data={actionAnimes} />
-      <AnimeSection titleSection="Romance" data={romanceAnimes}/>
-      <AnimeSection titleSection="Comédie" data={comedyAnimes}/>
+      <AnimeSection titleSection="Romance" data={romanceAnimes} />
+      <AnimeSection titleSection="Comédie" data={comedyAnimes} />
       <AnimeSection titleSection="Nouveautés" data={seasonAnimes} />
-      <AnimeSection titleSection="Les plus populaires" data={popularAnimes} />
 
     </>
   );
